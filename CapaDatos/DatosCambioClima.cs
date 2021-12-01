541e2ba74a985f4d4a5781645043c74210ce542c
aldomaz <aldomazrocket@gmail.com>
2021-11-30 23:50:17 -0500

    VisualizarClima

--- CapaDatos/DatosCambioClima.cs
+++ CapaDatos/DatosCambioClima.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using MySqlConnector;
 using CapaComun.Cache;
+using System.Data;
 
 namespace CapaDatos
 {
@@ -31,5 +32,37 @@ namespace CapaDatos
             comando.CommandType = System.Data.CommandType.Text;
             comando.ExecuteNonQuery();
         }
+
+        public DataTable CargarDGVclima()
+        {
+            MySqlConnection conexionBD = Conexion.conexion();
+            conexionBD.Open();
+            MySqlCommand comando = new MySqlCommand();
+            DataTable dt = new DataTable();
+
+            comando.Connection = conexionBD;
+            comando.CommandText = "select IDCAMBIOCLIMATICO as Item,LUGAR as Lugar,CAMBIOPOTENCIAL as 'Cambios Potenciales',AMENAZA as Amenazas,IMPACTOPREVISTO as 'Impactos Previstos',RIESGOCLIMATICO as 'Riesgo Climatico',ESTRATEGIASUGERIDA as 'Estrategias Sugeridas',POLITICAAPLICADA as 'Politicas en marcha' from cambioclimatico where IDCOMUNIDAD=@idcomunidad";
+            comando.Parameters.AddWithValue("@idcomunidad", CacheLoginComunidad.idcomunidad);
+            comando.CommandType = System.Data.CommandType.Text;
+            MySqlDataAdapter da = new MySqlDataAdapter(comando);
+            da.Fill(dt);
+            conexionBD.Close();
+            return dt;
+        }
+
+        public void EliminarClima(string item)
+        {
+            MySqlConnection conexionBD = Conexion.conexion();
+            conexionBD.Open();
+            MySqlCommand comando = new MySqlCommand();
+
+            comando.Connection = conexionBD;
+            comando.CommandText = "delete from cambioclimatico where IDCAMBIOCLIMATICO=@idcambioclimatico";
+            comando.Parameters.AddWithValue("@idcambioclimatico", item);
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.ExecuteNonQuery();
+            conexionBD.Close();
+        }
+
     }
 }
--- CapaPresentacion/frmContexto.cs
+++ CapaPresentacion/frmContexto.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CapaNegocio;
 
 namespace CapaPresentacion
 {
@@ -15,6 +16,52 @@ namespace CapaPresentacion
         public frmContexto()
         {
             InitializeComponent();
+        }
+
+        private void frmContexto_Load(object sender, EventArgs e)
+        {
+            ModeloCambioClima clima = new ModeloCambioClima();
+            dgvClima.DataSource = clima.CargarDGVclima();
+        }
+
+        private void btnModificarActor_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void dgvClima_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGuardarClima_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancelarClima_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEliminarClima_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("¿Desea eliminar ese actor, se eliminaran sus proyectos?", "Advertencia", MessageBoxButtons.YesNo);
+            ModeloCambioClima clima = new ModeloCambioClima();
+            if (result == DialogResult.Yes)
+            {
+                clima.EliminarClima(dgvClima.CurrentCell.Value.ToString());
+                dgvClima.DataSource = clima.CargarDGVclima();
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
