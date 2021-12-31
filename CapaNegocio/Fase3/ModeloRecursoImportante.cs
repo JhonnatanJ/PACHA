f67198eccc2e231914121826866629e209874f97
aldomaz <aldomazrocket@gmail.com>
2021-12-30 22:17:22 -0500

    Fase3 IngresarVisualizar

---
+++ CapaNegocio/Fase3/ModeloRecursoImportante.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos.Fase3;
+using System.Data;
+
+namespace CapaNegocio.Fase3
+{
+    public class ModeloRecursoImportante
+    {
+        DatosRecursoImportante datosRecursoImportante = new DatosRecursoImportante();
+
+        public void InsertarDatos(String sector, String tipoderecurso, String recurso, String cantidad, String calidad, String acceso, String control, String notas)
+        {
+            datosRecursoImportante.InsertarDatos(sector, tipoderecurso, recurso, cantidad, calidad, acceso, control, notas);
+        }
+
+        public DataTable CargarDGVrecursoImportante()
+        {
+            return datosRecursoImportante.CargarDGVrecursoImportante();
+        }
+
+        public void EliminarRecursoImportante(string item)
+        {
+            datosRecursoImportante.EliminarRecursoImportante(item);
+        }
+    }
+}
